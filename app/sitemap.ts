a7864995f63a1ca9ab637556b9b3c00d8e2eea2b
j-s-leee <q216044@gmail.com>
2025-02-23 13:57:37 +0900

    fix: sitemap err

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,6 @@
-import { fetchRSS } from "@/lib/fetchRSS";
 import type { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { items: posts } = await fetchRSS();
+export default function sitemap(): MetadataRoute.Sitemap {
   const sitemap: MetadataRoute.Sitemap = [
     {
       url: "https://ink-salt-bread.vercel.app",
@@ -11,14 +9,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
   ];
-  posts.map((post) => {
-    sitemap.push({
-      url: post.link!,
-      lastModified: post.pubDate!,
-      changeFrequency: "daily",
-      priority: 0.7,
-    });
-  });
 
   return sitemap;
 }
